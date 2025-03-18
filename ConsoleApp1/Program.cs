5abb0f2620198811cb7736c67689efbe72e51982
Le gia Nguyen <le.gia@elev.ga.ntig.se>
2025-03-18 14:03:09 +0100

    jagglömde att lämna in

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,45 +1,103 @@
 ﻿using System.Numerics;
 using Raylib_cs;
 
-Raylib.InitWindow(800, 800, "before and after");
+Raylib.InitWindow(400, 800, "before and after");
 Raylib.SetTargetFPS(60);
 
-Vector2 pos = new(200, 200);
+// alla information
+float radius = 20; // Ball radius
+int screenWidth = 400;
+int screenHeight = 800;
+string gameover;
 
+//all vector2
+Vector2 pos = new(200, 400);
+Vector2 top = new Vector2(150, 100);
+Vector2 bottomLeft = new Vector2(100, 150);
+Vector2 bottomRight = new Vector2(200, 150);
+
+// speed for all random block spawn
+List<Vector2> blocks = new();
+Random rand = new();
+float blockSpeed = 4;
+int blockSize = 30;
+
+int score = 0; 
+
+// spawn block random on screen width
+void SpawnBlock()
+{
+    blocks.Add(new Vector2(rand.Next(0, screenWidth - blockSize), 0));
+}
+
+// fråga chatgpt
+
+// keyboard setting
 while (!Raylib.WindowShouldClose())
 {
-  int mouseX = Raylib.GetMouseX();
-  int mouseY = Raylib.GetMouseY();
 
-  if (Raylib.IsKeyDown(KeyboardKey.Right))
-  {
-    pos.X += 3;
-  }
-  if (Raylib.IsKeyDown(KeyboardKey.Left))
-  {
-    pos.X -= 3;
-  }
-  if (Raylib.IsKeyDown(KeyboardKey.Up))
-  {
-    pos.Y -= 3;
-  }
-  if (Raylib.IsKeyDown(KeyboardKey.Down))
-  {
-    pos.Y += 3;
-  }
+    int mouseX = Raylib.GetMouseX();
+    int mouseY = Raylib.GetMouseY();
 
-  while (pos.Y == 800 || pos.X == 800)
-  {
-    pos.Y = 800;
-    pos.X = 800;
-  }
+    if (Raylib.IsKeyDown(KeyboardKey.Right) && pos.X + radius < screenWidth)
+    {
+        pos.X += 5; //to move right 
+    }
+    if (Raylib.IsKeyDown(KeyboardKey.Left) && pos.X - radius > 0)
+    {
+        pos.X -= 5; // to move left
+    }
+    if (Raylib.IsKeyDown(KeyboardKey.Up) && pos.Y - radius > 0)
+    {
+        pos.Y -= 5; // to move down
+    }
+    if (Raylib.IsKeyDown(KeyboardKey.Down) && pos.Y + radius < screenHeight)
+    {
+        pos.Y += 5; //to move up
+    }
+    if (rand.Next(100) < 10) // Randomly spawn blocks
+    {
+        SpawnBlock();
+    }
 
 
-  Raylib.BeginDrawing();
 
-  Raylib.ClearBackground(Color.White);
+    for (int i = 0; i < blocks.Count; i++)
+    {
+        blocks[i] = new Vector2(blocks[i].X, blocks[i].Y + blockSpeed);
+    }
 
-  Raylib.DrawCircleV(pos, 100, Color.Green);
+    blocks.RemoveAll(block => block.Y > screenHeight); //
 
-  Raylib.EndDrawing();
+    score++; //higher score every frame
+
+    // clear block after falling out of screen width and height
+
+    foreach (var block in blocks)
+    {
+        if (pos.X + radius > block.X && pos.X - radius < block.X + blockSize &&
+            pos.Y + radius > block.Y && pos.Y - radius < block.Y + blockSize)
+        {
+            blocks.Clear();
+            pos = new Vector2(200, 400); 
+            score = 0; // reset on collision
+        }
+    }
+
+    
+
+
+    Raylib.BeginDrawing();
+    Raylib.ClearBackground(Color.Gray); // backgroudn color gray
+    Raylib.DrawCircleV(pos, 20, Color.Black);
+
+    foreach (var block in blocks)
+    {
+        Raylib.DrawRectangleV(block, new Vector2(blockSize, blockSize), Color.Red);
+    }
+        // Draw Score on Screen
+    Raylib.DrawText($"Score: {score}", 10, 10, 20, Color.White);
+
+    Raylib.EndDrawing();
 }
+
--- ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81d3a26b3caa94d4d6f27ce9010e54e114e2ba8b")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
